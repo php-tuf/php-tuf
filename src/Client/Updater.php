a26cbfe3f8c6dc6856aca041a686c7c11f7a8621
Ted Bowman <ted@tedbow.com>
2020-08-05 15:29:12 -0400

    probably trash

--- src/Client/Updater.php
+++ src/Client/Updater.php
@@ -6,7 +6,13 @@ namespace Tuf\Client;
 
 class Updater
 {
-  public function sayHello() {
-    return 'hello';
+
+  /**
+   * @todo Update from python comment https://github.com/theupdateframework/tuf/blob/1cf085a360aaad739e1cc62fa19a2ece270bb693/tuf/client/updater.py#L999
+   *
+   * @param bool $unsafely_update_root_if_necessary
+   */
+  public function refresh($unsafely_update_root_if_necessary = TRUE) {
+
   }
 }
