a97752a865155c1adefa19307a049a872cf66c09
Mike Baynton <mike@mbaynton.com>
2020-08-05 15:02:37 -0500

    WIP webserver-in-tests
    Works with `composer test`; does not work with simple-phpunit / phpunit
    bridge

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -3,9 +3,24 @@
 
 use PHPUnit\Framework\TestCase;
 use Tuf\Client\Updater;
+use Tuf\Tests\Helpers\WebserverRunner;
 
 class HelloWorldTest extends TestCase
 {
+  static $webserver;
+
+  public static function setUpBeforeClass() : void
+  {
+    $x = new \Tuf\Tests\TestClass();
+    self::$webserver = new WebserverRunner();
+    self::$webserver->setUpBeforeClass("fixtures/tufrepo", 8001);
+  }
+  public static function tearDownAfterClass() : void
+  {
+    self::$webserver->tearDownAfterClass();
+  }
+
+
   public function testIsHello() {
     $u = new Updater();
     $this->assertEquals('hello', $u->sayHello());
---
+++ tests/TestClass.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Tuf\Tests;
+
+
+class TestClass
+{
+  public $thing = 1;
+}
