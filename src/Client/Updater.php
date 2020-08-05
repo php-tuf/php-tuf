66384cc40033da487c6fdee46ccedee14edde03b
Ted Bowman <ted@tedbow.com>
2020-08-05 16:10:27 -0400

    Updater test wip

--- src/Client/Updater.php
+++ src/Client/Updater.php
@@ -8,11 +8,52 @@ class Updater
 {
 
   /**
+   * @var string
+   */
+  protected $repoName;
+
+  /**
+   * @var \array[][]
+   */
+  protected $mirrors;
+
+  /**
+   * Updater constructor.
+   *
+   * @param string $repository_name
+   * @param array[][] $mirrors
+   *   Re
+   */
+  public function __construct($repository_name, $mirrors) {
+    $this->repoName = $repository_name;
+    $this->mirrors = $mirrors;
+  }
+
+
+  /**
    * @todo Update from python comment https://github.com/theupdateframework/tuf/blob/1cf085a360aaad739e1cc62fa19a2ece270bb693/tuf/client/updater.py#L999
    *
    * @param bool $unsafely_update_root_if_necessary
    */
   public function refresh($unsafely_update_root_if_necessary = TRUE) {
 
+  }
+
+  /**
+   * Validates a target.
+   *
+   * @param $target_repo_path
+   * @param $target_stream
+   *
+   * @return bool
+   *   Returns true if the target validates.
+   */
+  public function validateTarget($target_repo_path, $target_stream) {
+    $root_data = json_decode(fopen(__DIR__ . '/../../fixtures/tufclient/tufrepo/metadata/current/root.json'));
+    $version = (int) $root_data['signed']['version'];
+    $next_version = $version + 1;
+
+
+    return TRUE;
   }
 }
---
+++ tests/UpdaterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use Tuf\Client\Updater;
+
+class UpdaterTest extends TestCase {
+
+  public function testUpdateSuccessful() {
+    $mirrors = array(
+      'mirror1' => array(
+        'url_prefix' => 'http://localhost:8001',
+        'metadata_path' => 'metadata',
+        'targets_path' => 'targets',
+        'confined_target_dirs' => array(),
+      ),
+    );
+    $updater = new Updater('repo1', $mirrors);
+    $fixture_target = 'testtarget.txt';
+    $target_stream = fopen('data://text/plain,' . 'Test File', 'r');
+    $this->assertTrue($updater->validateTarget($fixture_target, $target_stream));
+
+
+  }
+
+  /**
+   * Tests that an error is thrown on an updated root.
+   *
+   * @todo Remove this test when functionality is added.
+   */
+  public function testUpdatedRootError() {
+
+  }
+
+}
