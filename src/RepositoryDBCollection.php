3f7571bdf91f57efd2bf02e3c92fee56c1b4f4e9
Mike Baynton <mike@mbaynton.com>
2020-08-06 14:58:32 -0500

    Signature verification happy path with test (#11)
    Added test and functionality for the basic verification
    
    Co-authored-by: Ted Bowman <ted@tedbow.com>
    Co-authored-by: xjm on Drupal.org <xjm@users.noreply.github.com>

---
+++ src/RepositoryDBCollection.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Tuf;
+
+/**
+ * Class RepositoryDBCollection
+ *
+ * Collects KeyDBs and RoleDBs by repository name.
+ */
+class RepositoryDBCollection
+{
+  /**
+   * ROLE_IX is the index of the repository array where the role DB is found.
+   */
+    const ROLE_IX = 0;
+  /**
+   * KEY_IX is the index of the repository array where the key DB is found.
+   */
+    const KEY_IX = 1;
+
+    protected $dbCollection = array();
+
+    protected static $singleton;
+
+    public static function singleton()
+    {
+        if (self::$singleton == null) {
+            self::$singleton = new self();
+        }
+
+        return self::$singleton;
+    }
+
+    public function setDatabasesForRepository(KeyDB $keyDB, RoleDB $roleDB, $repositoryName = 'default')
+    {
+        if (!empty($this->dbCollection[$repositoryName])) {
+            throw new \Exception("Repository already has databases: $repositoryName");
+        }
+
+        $this->dbCollection[$repositoryName] = array(
+        self::ROLE_IX => $roleDB,
+        self::KEY_IX => $keyDB,
+        );
+    }
+
+  /**
+   * @param string $repositoryName
+   * @return \array[]
+   *   Array containing role database at RepositoryDBCollection::ROLE_IX, key database at RepositoryDBCollection::KEY_IX
+   * @throws \Exception
+   */
+    public function getDatabasesForRepository($repositoryName = 'default')
+    {
+        if (empty($this->dbCollection[$repositoryName])) {
+            throw new \Exception("Repository name is not known: $repositoryName");
+        }
+
+        return $this->dbCollection[$repositoryName];
+    }
+}
