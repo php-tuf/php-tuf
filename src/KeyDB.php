d7d5c0b16b881eead00b6a932929e59dec6fe243
xjm <xjm@65776.no-reply.drupal.org>
2020-08-10 13:55:54 -0500

    Another badly indented comment.

--- src/KeyDB.php
+++ src/KeyDB.php
@@ -21,7 +21,7 @@ class KeyDB
                 // @todo Convert this to a log line as per Python.
                 throw new \Exception("Root metadata file contains an unsupported key type: \"${keyMeta['keytype']}\"");
             }
-          // One key id for each $keyMeta['keyid_hash_algorithms']
+            // One key id for each $keyMeta['keyid_hash_algorithms'].
             $computedKeyIds = self::computeKeyIds($keyMeta);
             foreach ($computedKeyIds as $keyId) {
                 $keyMeta['keyid'] = $keyId;
