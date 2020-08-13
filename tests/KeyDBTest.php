af60fdc4a8822f39323c97047145c0c0264e0ef1
xjm <xjm@65776.no-reply.drupal.org>
2020-08-13 12:38:03 -0500

    Label data provider arrays with parameter names.

--- tests/KeyDBTest.php
+++ tests/KeyDBTest.php
@@ -22,13 +22,13 @@ class KeyDBTest extends TestCase
     {
         return [
           "case 1" => [
-            0 => [
+            'metadata' => [
                   "keyid_hash_algorithms" => ['sha256', 'sha512'],
                   "keytype" => "ed25519",
                   "keyval" => ["public" => "edcd0a32a07dce33f7c7873aaffbff36d20ea30787574ead335eefd337e4dacd"],
                   "scheme" => "ed25519"
                 ],
-            1 => [
+            'expected' => [
               "59a4df8af818e9ed7abe0764c0b47b4240952aa0d179b5b78346c470ac30278d",
               "594e8b4bdafc33fd87e9d03a95be13a6dc93a836086614fd421116d829af68d8f0110ae93e3dde9a246897fd85171455ea53191b"
               . "b96cf9e589ba047d057dbd66"
