7fdba8e40ac3c234b611d9ab823e732778cf2fcc
Ted Bowman <ted@tedbow.com>
2020-08-14 14:29:04 -0400

    Copied docs for keyDb and roleDb

--- src/KeyDB.php
+++ src/KeyDB.php
@@ -3,6 +3,26 @@
 
 namespace Tuf;
 
+/**
+ * Represent a collection of keys and their organization.  This module ensures
+ * the layout of the collection remain consistent and easily verifiable.
+ * Provided are functions to add and delete keys from the database, retrieve a
+ * single key, and assemble a collection from keys stored in TUF 'Root'
+ * Metadata. The Update Framework process maintains a set of role info for
+ * multiple repositories.
+ *
+ * RSA keys are currently supported and a collection of keys is organized as a
+ * dictionary indexed by key ID.  Key IDs are used as identifiers for keys
+ * (e.g., RSA key).  They are the hexadecimal representations of the hash of
+ * key
+ * objects (specifically, the key object containing only the public key).  See
+ * 'rsa_key.py' and the '_get_keyid()' function to learn precisely how keyids
+ * are generated.  One may get the keyid of a key object by simply accessing
+ * the
+ * dictionary's 'keyid' key (i.e., rsakey['keyid']).
+ *
+ * @see https://github.com/theupdateframework/tuf/blob/292b18926b45106b27f582dc3cb1433363d03a9a/tuf/keydb.py
+ */
 class KeyDB
 {
     /**
@@ -12,6 +32,21 @@ class KeyDB
      */
     protected $keys;
 
+    /**
+     * Populate the key database with the unique keys found in 'root_metadata'.
+     * The database dictionary will conform to
+     * 'tuf.formats.KEYDB_SCHEMA' and have the form: {keyid: key,
+     * ...}.  The 'keyid' conforms to 'securesystemslib.formats.KEYID_SCHEMA' and
+     * 'key' to its respective type.  In the case of RSA keys, this object would
+     * match 'RSAKEY_SCHEMA'.
+     *
+     * @param $rootMetadata
+     *    A dictionary conformant to 'tuf.formats.ROOT_SCHEMA'.  The keys found
+     *    in the 'keys' field of 'root_metadata' are needed by this function.
+     *
+     * @return \Tuf\KeyDB
+     * @throws \Exception
+     */
     public static function createKeyDBFromRootMetadata($rootMetadata)
     {
         $db = new self();
@@ -60,11 +95,32 @@ class KeyDB
         $this->keys = [];
     }
 
+    /**
+     * Add 'rsakey_dict' to the key database while avoiding duplicates.
+     * If keyid is provided, verify it is the correct keyid for 'rsakey_dict'
+     * and raise an exception if it is not.
+     *
+     * @param $keyMeta
+     */
     public function addKey($keyMeta)
     {
         $this->keys[$keyMeta['keyid']] = $keyMeta;
     }
 
+    /**
+     * Return the key belonging to 'keyid'.
+     *
+     * @param $keyId
+     *     An object conformant to 'securesystemslib.formats.KEYID_SCHEMA'. It
+     *     is used as an identifier for keys.
+     *
+     * @return array
+     *     The key matching 'keyid'.  In the case of RSA keys, a dictionary
+     *     conformant to 'securesystemslib.formats.RSAKEY_SCHEMA' is returned.
+     *
+     * @throws \Exception
+     *     Thrown if 'keyid' is not found in the keydb database.
+     */
     public function getKey($keyId)
     {
         if (empty($this->keys[$keyId])) {
--- src/RoleDB.php
+++ src/RoleDB.php
@@ -3,15 +3,53 @@
 
 namespace Tuf;
 
+/**
+ * Represent a collection of roles and their organization.  The caller may
+ * create a collection of roles from those found in the 'root.json' metadata
+ * file by calling 'create_roledb_from_root_metadata()', or individually by
+ * adding roles with 'add_role()'.  There are many supplemental functions
+ * included here that yield useful information about the roles contained in the
+ * database, such as extracting all the parent rolenames for a specified
+ * rolename, deleting all the delegated roles, retrieving role paths, etc.  The
+ * Update Framework process maintains a role database for each repository.
+ *
+ * The role database is a dictionary conformant to
+ * 'tuf.formats.ROLEDICT_SCHEMA' and has the form:
+ *
+ * {'repository_name': {
+ * 'rolename': {'keyids': ['34345df32093bd12...'],
+ * 'threshold': 1
+ * 'signatures': ['abcd3452...'],
+ * 'paths': ['role.json'],
+ * 'path_hash_prefixes': ['ab34df13'],
+ * 'delegations': {'keys': {}, 'roles': {}}}
+ *
+ * The 'name', 'paths', 'path_hash_prefixes', and 'delegations' dict keys are
+ * optional.
+ *
+ * @see https://github.com/theupdateframework/tuf/blob/6ae3ea6d7d2aa80ba0571503a5e6c3808c44ff64/tuf/roledb.py
+ */
 class RoleDB
 {
     /**
-     * @var \array[]
-     *
      * Key roles indexed by role name.
+     *
+     * @var \array[]
      */
     protected $roles;
 
+    /**
+     *  Create a role database containing all of the unique roles found in
+     *  root_metadata'.
+     *
+     * @param $rootMetadata
+     *     A dictionary conformant to 'tuf.formats.ROOT_SCHEMA'.  The roles
+     *     found in the 'roles' field of 'root_metadata' is needed by this
+     *     function.
+     *
+     * @return \Tuf\RoleDB
+     * @throws \Exception
+     */
     public static function createRoleDBFromRootMetadata($rootMetadata)
     {
         $db = new self();
@@ -52,6 +90,31 @@ class RoleDB
         $this->roles = [];
     }
 
+    /**
+     * Add to the role database the 'roleinfo' associated with 'rolename'.
+     *
+     * @param $roleName
+     *     An object representing the role's name, conformant to 'ROLENAME_SCHEMA'
+     *     (e.g., 'root', 'snapshot', 'timestamp').
+     * @param $roleInfo
+     *     An object representing the role associated with 'rolename', conformant to
+     *     ROLEDB_SCHEMA.  'roleinfo' has the form:
+     *
+     *     {'keyids': ['34345df32093bd12...'],
+     *     'threshold': 1,
+     *     'signatures': ['ab23dfc32']
+     *     'paths': ['path/to/target1', 'path/to/target2', ...],
+     *     'path_hash_prefixes': ['a324fcd...', ...],
+     *     'delegations': {'keys': }
+     *
+     *     The 'paths', 'path_hash_prefixes', and 'delegations' dict keys are
+     *     optional.
+     *
+     *     The 'target' role has an additional 'paths' key.  Its value is a list of
+     *     strings representing the path of the target file(s).
+     *
+     * @throws \Exception
+     */
     public function addRole($roleName, $roleInfo)
     {
         if ($this->roleExists($roleName)) {
@@ -61,6 +124,16 @@ class RoleDB
         $this->roles[$roleName] = $roleInfo;
     }
 
+    /**
+     * Verify whether 'rolename' is stored in the role database.
+     *
+     * @param $roleName
+     *     An object representing the role's name, conformant to 'ROLENAME_SCHEMA'
+     *     (e.g., 'root', 'snapshot', 'timestamp').
+     *
+     * @return bool
+     *     True if 'rolename' is found in the role database, False otherwise.
+     */
     public function roleExists($roleName)
     {
         return !empty($this->roles[$roleName]);
@@ -76,8 +149,19 @@ class RoleDB
     }
 
     /**
+     * Return a list of the keyids associated with 'rolename'.  Keyids are used as
+     * identifiers for keys (e.g., rsa key).  A list of keyids are associated with
+     * each rolename.  Signing a metadata file, such as 'root.json' (Root role),
+     * involves signing or verifying the file with a list of keys identified by
+     * keyid.
+     *
      * @param string $roleName
+     *    An object representing the role's name, conformant to
+     *    'ROLENAME_SCHEMA' (e.g., 'root', 'snapshot', 'timestamp').
+     *
      * @return \array[]
+     *    A list of keyids.
+     *
      * @throws \Exception
      */
     public function getRoleKeyIds($roleName)
