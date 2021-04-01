4c3ec3acee3a3d7cfd75fe3ed1f2923483d2f15d
Phéna Proxima <adam@phenaproxima.net>
2021-04-01 13:34:02 -0400

    Make InvalidHashException aware of the offending stream

--- src/Exception/PotentialAttackException/InvalidHashException.php
+++ src/Exception/PotentialAttackException/InvalidHashException.php
@@ -2,6 +2,7 @@
 
 namespace Tuf\Exception\PotentialAttackException;
 
+use Psr\Http\Message\StreamInterface;
 use Tuf\Exception\TufException;
 
 /**
@@ -9,4 +10,39 @@ use Tuf\Exception\TufException;
  */
 class InvalidHashException extends TufException
 {
+    /**
+     * A stream object pointing to the downloaded target.
+     *
+     * @var \Psr\Http\Message\StreamInterface
+     */
+    private $stream;
+
+    /**
+     * InvalidHashException constructor.
+     *
+     * @param \Psr\Http\Message\StreamInterface $stream
+     *   A stream object pointing to the downloaded target.
+     * @param string $message
+     *   (optional) The exception message.
+     * @param int $code
+     *   (optional) The exception code.
+     * @param \Throwable|null $previous
+     *   The previous exception, if any.
+     */
+    public function __construct(StreamInterface $stream, $message = "", $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->stream = $stream;
+    }
+
+    /**
+     * Returns the stream object pointing to the downloaded target.
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     *   The stream object.
+     */
+    public function getStream(): StreamInterface
+    {
+        return $this->stream;
+    }
 }
