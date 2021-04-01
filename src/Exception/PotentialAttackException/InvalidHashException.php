b03ff0229136d70def12a23b86f53aeb5bf82395
Phéna Proxima <adam@phenaproxima.net>
2021-04-01 14:45:58 -0400

    Even scarier

--- src/Exception/PotentialAttackException/InvalidHashException.php
+++ src/Exception/PotentialAttackException/InvalidHashException.php
@@ -9,18 +9,18 @@ use Tuf\Exception\TufException;
  * Indicates an invalid hash was computed for a downloaded target.
  *
  * This exception provides access to the underlying stream which caused the
- * error. The stream's contents are NOT trusted by TUF, so any code which
- * interacts with this exception, or the stream, should do so with great caution
- * and treat the stream contents as unsafe.
+ * error. The stream's contents have failed TUF validation, so any code which
+ * interacts with the stream should do so with great caution and treat its
+ * contents as unsafe.
  */
 class InvalidHashException extends TufException
 {
     /**
      * An untrusted stream object pointing to the downloaded target.
      *
-     * WARNING: The contents of the stream are NOT trusted by TUF! Any code
-     * interacting with this exception, or the underlying stream, should proceed
-     * with great caution.
+     * WARNING: The contents of the stream failed TUF validation. Any code
+     * interacting with this stream, should treat it as unsafe and proceed with
+     * great caution.
      *
      * @var \Psr\Http\Message\StreamInterface
      */
@@ -31,8 +31,9 @@ class InvalidHashException extends TufException
      *
      * @param \Psr\Http\Message\StreamInterface $stream
      *   An untrusted stream object pointing to the downloaded target. The
-     *   contents of this stream are NOT trusted by TUF! Any code interacting
-     *   with this exception, or this stream, should proceed with great caution.
+     *   contents of this stream have failed TUF validation, so any code
+     *   interacting with it should treat it as unsafe and proceed with great
+     *   caution.
      * @param string $message
      *   (optional) The exception message.
      * @param int $code
@@ -49,9 +50,8 @@ class InvalidHashException extends TufException
     /**
      * Returns the untrusted stream object pointing to the downloaded target.
      *
-     * WARNING: The contents of the stream are NOT trusted by TUF! Any code
-     * interacting with this exception, or the underlying stream, should proceed
-     * with great caution.
+     * WARNING: The contents of the stream failed TUF validation. Any code
+     * interacting it should treat it as unsafe and proceed with great caution.
      *
      * @return \Psr\Http\Message\StreamInterface
      *   The stream object.
