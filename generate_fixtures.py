571fd1f821225462e0a4348945e88cb8d83069a7
David Strauss <david@davidstrauss.net>
2021-04-16 12:09:32 -0700

    Fixtures: Threshold = 2 and Related Attack (#165)
    Co-authored-by: pwolanin <pwolanin@49851.no-reply.drupal.org>

--- generate_fixtures.py
+++ generate_fixtures.py
@@ -4,6 +4,7 @@ from tuf import repository_tool as rt
 import os
 import shutil
 from unittest import mock
+import json
 
 # This file largely derives from the TUF tutorial:
 # https://github.com/theupdateframework/tuf/blob/develop/docs/TUTORIAL.md
@@ -186,11 +187,51 @@ class TUFTestFixtureDelegated(TUFTestFixtureSimple):
         self.write_and_publish_repository()
 
 
+class TUFTestFixtureThresholdTwo(TUFTestFixtureBase):
+    def __init__(self):
+        super().__init__()
+        (public_timestamp_key_2, private_timestamp_key_2) = self.write_and_import_keypair(
+            'timestamp_2')
+        self.repository.timestamp.add_verification_key(public_timestamp_key_2)
+        self.repository.timestamp.load_signing_key(private_timestamp_key_2)
+        self.repository.timestamp.threshold = 2
+        self.repository.mark_dirty(['timestamp'])
+        self.write_and_publish_repository(export_client=True)
+
+
+class TUFTestFixtureThresholdTwoAttack(TUFTestFixtureThresholdTwo):
+    def __init__(self):
+        super().__init__()
+        timestamp_path = os.path.join(self.tufrepo_dir, "metadata", "timestamp.json")
+        self.repository.mark_dirty(["timestamp"])
+        self.write_and_publish_repository(export_client=True)
+
+        # By exporting the repo but not the client, this gives us a new revision
+        # that's ready to alter. If we alter a version the client is already
+        # aware of, it may not pick up this new, altered version.
+        self.repository.mark_dirty(["timestamp"])
+        self.write_and_publish_repository(export_client=False)
+        with open(timestamp_path) as timestamp_fd:
+            timestamp = json.load(timestamp_fd)
+
+        first_sig = timestamp["signatures"][0]
+        timestamp["signatures"] = [first_sig, first_sig]
+
+        with open(timestamp_path, "w") as timestamp_fd:
+            json.dump(timestamp, timestamp_fd, indent=1)
+
+        # We could also alter the versioned (N.timestamp.json), but the spec
+        # considers these as optional, so we can expect this alteration to be
+        # sufficient.
+
+
 @mock.patch('time.time', mock.MagicMock(return_value=1577836800))
 def generate_fixtures():
     TUFTestFixtureSimple()
     TUFTestFixtureDelegated()
     TUFTestFixtureAttackRollback()
+    TUFTestFixtureThresholdTwo()
+    TUFTestFixtureThresholdTwoAttack()
 
 
 generate_fixtures()
