95bf6ffa5da74b110b8cd2955b92f0d49a49baae
Anthony Fournier <anthony@octopusride.com>
2021-04-16 09:32:01 -0400

    152: Added return type declarations to methods in the KeyDB class.

--- src/KeyDB.php
+++ src/KeyDB.php
@@ -44,7 +44,7 @@ class KeyDB
      *
      * @see https://github.com/theupdateframework/specification/blob/v1.0.9/tuf-spec.md#4-document-formats
      */
-    public static function createFromRootMetadata(RootMetadata $rootMetadata, bool $allowUntrustedAccess = false)
+    public static function createFromRootMetadata(RootMetadata $rootMetadata, bool $allowUntrustedAccess = false): KeyDB
     {
         $db = new self();
 
@@ -63,7 +63,7 @@ class KeyDB
      *
      * @see src/constants.php
      */
-    public static function getSupportedKeyTypes()
+    public static function getSupportedKeyTypes(): array
     {
         static $types = [];
         if (count($types) == 0) {
@@ -89,7 +89,7 @@ class KeyDB
      *
      * @todo https://github.com/php-tuf/php-tuf/issues/56
      */
-    private static function computeKeyIds(\ArrayAccess $keyMeta)
+    private static function computeKeyIds(\ArrayAccess $keyMeta): array
     {
         $keyCanonicalStruct = [
             'keytype' => $keyMeta['keytype'],
@@ -131,7 +131,7 @@ class KeyDB
      *
      * @todo https://github.com/php-tuf/php-tuf/issues/56
      */
-    private function addKey(\ArrayAccess $keyMeta)
+    private function addKey(\ArrayAccess $keyMeta): void
     {
         if (! in_array($keyMeta['keytype'], self::getSupportedKeyTypes(), true)) {
             // @todo Convert this to a log line as per Python.
@@ -159,7 +159,7 @@ class KeyDB
      *
      * @see https://github.com/theupdateframework/specification/blob/v1.0.9/tuf-spec.md#4-document-formats
      */
-    public function getKey(string $keyId):\ArrayObject
+    public function getKey(string $keyId): \ArrayObject
     {
         if (empty($this->keys[$keyId])) {
             throw new NotFoundException($keyId, 'key');
