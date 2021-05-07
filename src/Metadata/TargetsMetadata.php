6d4f78264acf98c9bfd3764ac9c60621100a8296
Ted Bowman <tedbow@users.noreply.github.com>
2021-05-07 12:22:13 -0400

    make delegated role class (#183)

--- src/Metadata/TargetsMetadata.php
+++ src/Metadata/TargetsMetadata.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Optional;
 use Symfony\Component\Validator\Constraints\Required;
 use Symfony\Component\Validator\Constraints\Type;
+use Tuf\DelegatedRole;
 use Tuf\Exception\NotFoundException;
 use Tuf\Key;
 
@@ -187,11 +188,16 @@ class TargetsMetadata extends MetadataBase
     /**
      * Gets the delegated roles if any.
      *
-     * @return \ArrayObject[]
+     * @return \Tuf\DelegatedRole[]
      *   The delegated roles.
      */
     public function getDelegatedRoles(): array
     {
-        return $this->getSigned()['delegations']['roles'] ?? [];
+        $roles = [];
+        foreach ($this->getSigned()['delegations']['roles'] as $roleInfo) {
+            $role = DelegatedRole::createFromMetadata($roleInfo);
+            $roles[$role->getName()] = $role;
+        }
+        return $roles;
     }
 }
