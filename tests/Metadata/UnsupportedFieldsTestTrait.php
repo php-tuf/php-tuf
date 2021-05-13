71787975c6b7572bd412f871aa3137ff2c1d1e0f
Ted Bowman <ted@tedbow.com>
2021-05-13 10:57:41 -0400

    forgot add UnsupportedFieldsTestTrait

---
+++ tests/Metadata/UnsupportedFieldsTestTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tuf\Tests\Metadata;
+
+use Tuf\Exception\MetadataException;
+
+/**
+ * Test method for testing unsupported fields.
+ */
+trait UnsupportedFieldsTestTrait
+{
+
+    /**
+     * Tests that unsupported fields throw an exception.
+     *
+     * @param array $unsupportedField
+     *   The array of nested keys for the unsupported field.
+     * @param mixed $fieldValue
+     *   The field value to set.
+     * @param string $expectedMessage
+     *   The expected regex exception message.
+     *
+     * @dataProvider providerUnsupportedFields
+     *
+     * @return void
+     */
+    public function testUnsupportedFields(array $unsupportedField, $fieldValue, string $expectedMessage): void
+    {
+        $metadata = json_decode($this->localRepo[$this->validJson], true);
+        $this->nestedChange($unsupportedField, $metadata, $fieldValue);
+        $fieldName = array_pop($unsupportedField);
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessageMatches("/$expectedMessage/s");
+        static::callCreateFromJson(json_encode($metadata));
+    }
+
+    /**
+     * Data provider for testUnsupportedFields().
+     *
+     * @return array[]
+     *  The test cases.
+     */
+    abstract public function providerUnsupportedFields(): array;
+}
