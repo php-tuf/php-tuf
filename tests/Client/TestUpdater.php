87049a4f1e810fa6790489ad7456c3faddfa3f33
Ted Bowman <ted@tedbow.com>
2021-05-18 16:58:17 -0400

    pass Clock into TestUpdater

--- tests/Client/TestUpdater.php
+++ tests/Client/TestUpdater.php
@@ -4,16 +4,17 @@ namespace Tuf\Tests\Client;
 
 use Tuf\Client\RepoFileFetcherInterface;
 use Tuf\Client\Updater;
+use Tuf\Helper\Clock;
 use Tuf\Tests\TestHelpers\TestClock;
 
 /**
- * An updater class for testing using the \Tuf\Tests\TestHelpers\TestClock class.
+ * An updater class for testing that allows specifying the Clock instance to use.
  */
 class TestUpdater extends Updater
 {
-    public function __construct(RepoFileFetcherInterface $repoFileFetcher, array $mirrors, \ArrayAccess $durableStorage)
+    public function __construct(RepoFileFetcherInterface $repoFileFetcher, array $mirrors, \ArrayAccess $durableStorage, Clock $testClock)
     {
         parent::__construct($repoFileFetcher, $mirrors, $durableStorage);
-        $this->clock = new TestClock();
+        $this->clock = $testClock;
     }
 }
