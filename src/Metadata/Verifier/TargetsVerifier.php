4131ae9653b163505660d323ed5469fef402ea9a
phenaproxima <adam@phenaproxima.net>
2021-05-21 15:30:03 -0400

    Document TargetsVerifier

--- src/Metadata/Verifier/TargetsVerifier.php
+++ src/Metadata/Verifier/TargetsVerifier.php
@@ -6,10 +6,25 @@ use Tuf\Client\SignatureVerifier;
 use Tuf\Metadata\MetadataBase;
 use Tuf\Metadata\SnapshotMetadata;
 
+/**
+ * Verifier for targets metadata.
+ */
 class TargetsVerifier extends VerifierBase
 {
     use TrustedAuthorityTrait;
 
+    /**
+     * TargetsVerifier constructor.
+     *
+     * @param SignatureVerifier $signatureVerifier
+     *   The signature verifier.
+     * @param \DateTimeImmutable $expiration
+     *   The time beyond which untrusted metadata will be considered expired.
+     * @param MetadataBase|null $trustedMetadata
+     *   The trusted metadata, if any.
+     * @param SnapshotMetadata|null $snapshotMetadata
+     *   The trusted snapshot metadata, if any.
+     */
     public function __construct(SignatureVerifier $signatureVerifier, \DateTimeImmutable $expiration, MetadataBase $trustedMetadata = null, SnapshotMetadata $snapshotMetadata = null)
     {
         parent::__construct($signatureVerifier, $expiration, $trustedMetadata);
@@ -32,6 +47,5 @@ class TargetsVerifier extends VerifierBase
 
         // TUF-SPEC-v1.0.16 Section 5.5.4
         static::checkFreezeAttack($untrustedMetadata, $this->metadataExpiration);
-        $untrustedMetadata->setIsTrusted(true);
     }
 }
