c9db3f71c5b9014687feb236462ab143c114f3d3
Ted Bowman <ted@tedbow.com>
2021-07-12 11:54:22 -0400

    hash ignorning files except client and server

--- fixtures/builder.py
+++ fixtures/builder.py
@@ -45,10 +45,8 @@ class FixtureBuilder:
         # Create a hash for the generated fixture. Generate the hash before
         # we open the `hash.txt` file because the empty file will affect the
         # hash of directory.
-        hashes = 'client:' + dirhash(self.dir + '/client', 'sha256')
-        hashes += "\nserver:" + dirhash(self.dir + '/server', 'sha256')
         with open(self.dir + "/hash.txt", "w") as hash_file:
-            hash_file.write(hashes)
+            hash_file.write(dirhash(self.dir, 'sha256', ignore=["__init.py", "hash.txt"]))
 
     def _role(self, name):
         """Loads a role object for a specific role."""
