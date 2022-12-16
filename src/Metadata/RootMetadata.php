de6ccc2ea8beefbcc6f5928eaebb9c305d9b33ab
Ted Bowman <ted+git@tedbow.com>
2022-12-16 11:59:56 -0500

    phpcbf

--- src/Metadata/RootMetadata.php
+++ src/Metadata/RootMetadata.php
@@ -5,7 +5,6 @@ namespace Tuf\Metadata;
 use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Validator\Constraints\Count;
-use Symfony\Component\Validator\Constraints\EqualTo;
 use Symfony\Component\Validator\Constraints\Optional;
 use Symfony\Component\Validator\Constraints\Required;
 use Symfony\Component\Validator\Constraints\Type;
