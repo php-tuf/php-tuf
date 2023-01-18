ceea3304b9f92d84b5ea22d82dbb7fb4a52d58ed
Phéna Proxima <adam@phenaproxima.net>
2023-01-18 12:47:50 -0500

    Create variants of TUFTestFixtureAttackRollback

--- fixtures/TUFTestFixtureAttackRollback/__init__.py
+++ fixtures/TUFTestFixtureAttackRollback/__init__.py
@@ -1,21 +1,28 @@
 from fixtures.builder import FixtureBuilder
 
+import os
 import shutil
 
 
 def build():
-    fixture = FixtureBuilder('TUFTestFixtureAttackRollback')\
-        .create_target('testtarget.txt')\
-        .publish(with_client=True)
+    variants = {
+        'consistent': True,
+        'inconsistent': False
+    }
+    for suffix, consistent in variants.items():
+        name = os.path.join('TUFTestFixtureAttackRollback', suffix)
+        fixture = FixtureBuilder(name)\
+            .create_target('testtarget.txt')\
+            .publish(with_client=True, consistent=consistent)
 
-    server_dir = fixture._server_dir
-    backup_dir = server_dir + '_backup'
-    shutil.copytree(server_dir, backup_dir, dirs_exist_ok=True)
+        server_dir = fixture._server_dir
+        backup_dir = server_dir + '_backup'
+        shutil.copytree(server_dir, backup_dir, dirs_exist_ok=True)
 
-    fixture.create_target('testtarget2.txt')\
-        .publish(with_client=True)
-    shutil.rmtree(server_dir + '/')
+        fixture.create_target('testtarget2.txt')\
+            .publish(with_client=True)
+        shutil.rmtree(server_dir + '/')
 
-    # Reset the client to previous state to simulate a rollback attack.
-    shutil.copytree(backup_dir, server_dir, dirs_exist_ok=True)
-    shutil.rmtree(backup_dir + '/')
+        # Reset the client to previous state to simulate a rollback attack.
+        shutil.copytree(backup_dir, server_dir, dirs_exist_ok=True)
+        shutil.rmtree(backup_dir + '/')
