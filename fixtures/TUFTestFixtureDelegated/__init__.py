fb653fdfe6f2f9b0c7b17c2b00dfc8827e3c0ebb
Phéna Proxima <adam@phenaproxima.net>
2023-01-18 12:50:55 -0500

    Also TUFTestFixtureDelegated

--- fixtures/TUFTestFixtureDelegated/__init__.py
+++ fixtures/TUFTestFixtureDelegated/__init__.py
@@ -1,21 +1,29 @@
 from fixtures.builder import FixtureBuilder
 
+import os
+
 
 def build():
-    fixture = FixtureBuilder('TUFTestFixtureDelegated')\
-        .create_target('testtarget.txt')\
-        .publish(with_client=True)\
-        .delegate('unclaimed', ['level_1_*.txt'])\
-        .create_target('level_1_target.txt', signing_role='unclaimed')\
-        .publish(with_client=True)
-    # === Point of No Return ===
-    # Past this point, we don't re-export the client. This supports testing the
-    # client's own ability to pick up and trust new data from the repository.
-    fixture.add_key('targets')\
-        .add_key('snapshot')\
-        .invalidate()\
-        .publish()\
-        .revoke_key('targets')\
-        .revoke_key('snapshot')\
-        .invalidate()\
-        .publish()
+    variants = {
+        'consistent': True,
+        'inconsistent': False
+    }
+    for suffix, consistent in variants.items():
+        name = os.path.join('TUFTestFixtureDelegated', suffix)
+        fixture = FixtureBuilder(name)\
+            .create_target('testtarget.txt')\
+            .publish(with_client=True, consistent=consistent)\
+            .delegate('unclaimed', ['level_1_*.txt'])\
+            .create_target('level_1_target.txt', signing_role='unclaimed')\
+            .publish(with_client=True, consistent=consistent)
+        # === Point of No Return ===
+        # Past this point, we don't re-export the client. This supports testing the
+        # client's own ability to pick up and trust new data from the repository.
+        fixture.add_key('targets')\
+            .add_key('snapshot')\
+            .invalidate()\
+            .publish(consistent=consistent)\
+            .revoke_key('targets')\
+            .revoke_key('snapshot')\
+            .invalidate()\
+            .publish(consistent=consistent)
