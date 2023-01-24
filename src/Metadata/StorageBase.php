78e72f23e97ab0a92694cbde0f8996d59bbd7e99
Phéna Proxima <adam@phenaproxima.net>
2023-01-24 15:21:22 -0500

    docs

--- src/Metadata/StorageBase.php
+++ src/Metadata/StorageBase.php
@@ -2,12 +2,37 @@
 
 namespace Tuf\Metadata;
 
+/**
+ * Defines a base class for saving and loading trusted TUF metadata.
+ */
 abstract class StorageBase implements StorageInterface
 {
+    /**
+     * Reads raw metadata.
+     *
+     * @param string $name
+     *   The name of the metadata to read (e.g., `root`, `targets`), without
+     *   file extension.
+     *
+     * @return string|null
+     *   The metadata, as a string, or null if the metadata is unavailable.
+     */
     abstract protected function read(string $name): ?string;
 
+    /**
+     * Writes trusted metadata as a string.
+     *
+     * @param string $name
+     *   The name of the metadata to write (e.g., `root`, `targets`), without
+     *   file extension.
+     * @param string $data
+     *   The metadata to write, fully encoded and normalized as desired.
+     */
     abstract protected function write(string $name, string $data): void;
 
+    /**
+     * {@inheritdoc}
+     */
     public function getRoot(): RootMetadata
     {
         $data = $this->read(RootMetadata::TYPE);
@@ -17,42 +42,63 @@ abstract class StorageBase implements StorageInterface
         throw new \LogicException("Could not load root metadata.");
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setRoot(RootMetadata $metadata): void
     {
         $metadata->ensureIsTrusted();
         $this->write(RootMetadata::TYPE, $metadata->getSource());
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getTimestamp(): ?TimestampMetadata
     {
         $data = $this->read(TimestampMetadata::TYPE);
         return $data ? TimestampMetadata::createFromJson($data)->trust() : null;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setTimestamp(TimestampMetadata $metadata): void
     {
         $metadata->ensureIsTrusted();
         $this->write(TimestampMetadata::TYPE, $metadata->getSource());
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getSnapshot(): ?SnapshotMetadata
     {
         $data = $this->read(SnapshotMetadata::TYPE);
         return $data ? SnapshotMetadata::createFromJson($data)->trust() : null;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setSnapshot(SnapshotMetadata $metadata): void
     {
         $metadata->ensureIsTrusted();
         $this->write(SnapshotMetadata::TYPE, $metadata->getSource());
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getTargets(string $role = 'targets'): ?TargetsMetadata
     {
         $data = $this->read($role);
         return $data ? TargetsMetadata::createFromJson($data, $role)->trust() : null;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setTargets(TargetsMetadata $metadata): void
     {
         $metadata->ensureIsTrusted();
--- src/Metadata/StorageInterface.php
+++ src/Metadata/StorageInterface.php
@@ -2,23 +2,87 @@
 
 namespace Tuf\Metadata;
 
+/**
+ * Defines an interface for saving and loading trusted TUF metadata.
+ */
 interface StorageInterface
 {
+    /**
+     * Loads trusted root metadata.
+     *
+     * @return \Tuf\Metadata\RootMetadata
+     *   The trusted root metadata.
+     *
+     * @throws \LogicException
+     *   Thrown if the root metadata cannot be loaded.
+     */
     public function getRoot(): RootMetadata;
 
+    /**
+     * Persists the trusted root metadata.
+     *
+     * @param \Tuf\Metadata\RootMetadata $metadata
+     *   The root metadata to persist. Must be trusted.
+     */
     public function setRoot(RootMetadata $metadata): void;
 
+    /**
+     * Loads trusted timestamp metadata.
+     *
+     * @return \Tuf\Metadata\TimestampMetadata|null
+     *   The trusted timestamp metadata, or null if none is available.
+     */
     public function getTimestamp(): ?TimestampMetadata;
 
+    /**
+     * Persists the trusted timestamp metadata.
+     *
+     * @param \Tuf\Metadata\TimestampMetadata $metadata
+     *   The timestamp metadata to persist. Must be trusted.
+     */
     public function setTimestamp(TimestampMetadata $metadata): void;
 
+    /**
+     * Loads trusted snapshot metadata.
+     *
+     * @return \Tuf\Metadata\SnapshotMetadata|null
+     *   The trusted snapshot metadata, or null if none is available.
+     */
     public function getSnapshot(): ?SnapshotMetadata;
 
+    /**
+     * Persists the trusted snapshot metadata.
+     *
+     * @param \Tuf\Metadata\SnapshotMetadata $metadata
+     *   The snapshot metadata to persist. Must be trusted.
+     */
     public function setSnapshot(SnapshotMetadata $metadata): void;
 
+    /**
+     * Loads trusted targets metadata for a specific role.
+     *
+     * @param string $role
+     *   (optional) The role to load. Defaults to `targets`.
+     *
+     * @return \Tuf\Metadata\TargetsMetadata|null
+     *   The trusted targets metadata, or null if none is available.
+     */
     public function getTargets(string $role = 'targets'): ?TargetsMetadata;
 
+    /**
+     * Persists trusted targets metadata.
+     *
+     * @param \Tuf\Metadata\TargetsMetadata $metadata
+     *   The targets metadata to persist. Can be for the root `targets` role
+     *   or a delegated role, but must be trusted in any case.
+     */
     public function setTargets(TargetsMetadata $metadata): void;
 
+    /**
+     * Deletes stored metadata.
+     *
+     * @param string $name
+     *   The name of the metadata to delete, without file extension.
+     */
     public function delete(string $name): void;
 }
