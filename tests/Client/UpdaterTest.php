bc03e3e0dfe323b981deb4495337f4490374fd2f
Phéna Proxima <adam@phenaproxima.net>
2023-02-02 08:24:04 -0500

    restore remvoed coverage

--- tests/Client/UpdaterTest.php
+++ tests/Client/UpdaterTest.php
@@ -1387,6 +1387,44 @@ abstract class UpdaterTest extends TestCase
         $this->assertSame($expectedLength, $this->serverStorage->fileSizes[$downloadedFileName]);
     }
 
+    public function providerMetadataTooBig(): array
+    {
+        return [
+            'snapshot.json too big' => [
+                'Simple',
+                'timestamp.json',
+                'snapshot.json',
+            ],
+            'targets.json too big' => [
+                'TargetsLengthNoSnapshotLength',
+                'snapshot.json',
+                'targets.json',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerMetadataTooBig
+     *
+     * @testdox Exception if $fileToChange is bigger than stated in $authorityFile
+     */
+    public function testMetadataFileTooBig(string $fixtureName, string $authorityFile, string $fileToChange): void
+    {
+        $updater = $this->getSystemInTest($fixtureName);
+
+        $authorityData = file_get_contents(static::getFixturePath($fixtureName, "server/metadata/$authorityFile", false));
+        $authorityData = json_decode($authorityData, true);
+        // Even if consistent snapshots are used, the authority file does not
+        // use the version prefix when referring to the file to change.
+        $fileToChangeKey = ltrim($fileToChange, '.0123456789');
+        $knownLength = $authorityData['signed']['meta'][$fileToChangeKey]['length'];
+        $this->serverStorage->set($fileToChange, str_repeat('a', $knownLength + 1));
+
+        $this->expectException(DownloadSizeException::class);
+        $this->expectExceptionMessage("$fileToChange exceeded $knownLength bytes");
+        $updater->refresh();
+    }
+
     public function testSnapshotHashes(string $targetsFileName = 'targets.json'): void
     {
         $updater = $this->getSystemInTest('Simple_WithHashes');
