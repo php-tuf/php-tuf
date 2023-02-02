b39b1ad5d8319efbb344059b6e69c474d7cbae0c
Phéna Proxima <adam@phenaproxima.net>
2023-02-02 15:55:04 -0500

    Remove MaxBytesSpyLoader

--- tests/Unit/RepositoryTest.php
+++ tests/Unit/RepositoryTest.php
@@ -3,7 +3,9 @@
 namespace Tuf\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
 use Tuf\Client\Repository;
+use Tuf\Loader\LoaderInterface;
 use Tuf\Loader\SizeCheckingLoader;
 use Tuf\Metadata\RootMetadata;
 use Tuf\Metadata\SnapshotMetadata;
@@ -11,49 +13,61 @@ use Tuf\Metadata\TargetsMetadata;
 use Tuf\Metadata\TimestampMetadata;
 use Tuf\Tests\TestHelpers\FileLoader;
 use Tuf\Tests\TestHelpers\FixturesTrait;
-use Tuf\Tests\TestHelpers\MaxBytesSpyLoader;
 
 /**
  * @covers \Tuf\Client\Repository
  */
-class RepositoryTest extends TestCase
+class RepositoryTest extends TestCase implements LoaderInterface
 {
     use FixturesTrait;
+
+    private array $maxBytes = [];
+
+    private FileLoader $fileLoader;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(string $uri, int $maxBytes = null): StreamInterface
+    {
+        $this->maxBytes[$uri][] = $maxBytes;
+        return $this->fileLoader->load($uri, $maxBytes);
+    }
 
     public function testRepository(): void
     {
         $baseDir = static::getFixturePath('Delegated', 'consistent/server/metadata');
-        $loader = new MaxBytesSpyLoader(new FileLoader($baseDir));
-        $repository = new Repository(new SizeCheckingLoader($loader));
+        $this->fileLoader = new FileLoader($baseDir);
+        $repository = new Repository(new SizeCheckingLoader($this));
 
         $this->assertInstanceOf(RootMetadata::class, $repository->getRoot(1));
-        $this->assertSame(Repository::MAX_BYTES, $loader->maxBytes['1.root.json'][0]);
+        $this->assertSame(Repository::MAX_BYTES, $this->maxBytes['1.root.json'][0]);
 
         $this->assertNull($repository->getRoot(999));
-        $this->assertSame(Repository::MAX_BYTES, $loader->maxBytes['999.root.json'][0]);
+        $this->assertSame(Repository::MAX_BYTES, $this->maxBytes['999.root.json'][0]);
 
         $this->assertInstanceOf(TimestampMetadata::class, $repository->getTimestamp());
-        $this->assertSame(Repository::MAX_BYTES, $loader->maxBytes['timestamp.json'][0]);
+        $this->assertSame(Repository::MAX_BYTES, $this->maxBytes['timestamp.json'][0]);
 
         foreach ([1, null] as $version) {
             $fileName = isset($version) ? "$version.snapshot.json" : 'snapshot.json';
 
             $this->assertInstanceOf(SnapshotMetadata::class, $repository->getSnapshot($version));
-            $this->assertSame(Repository::MAX_BYTES, $loader->maxBytes[$fileName][0]);
+            $this->assertSame(Repository::MAX_BYTES, $this->maxBytes[$fileName][0]);
 
             $fileSize = filesize($baseDir . '/' . $fileName);
             $this->assertInstanceOf(SnapshotMetadata::class, $repository->getSnapshot($version, $fileSize));
-            $this->assertSame($fileSize, $loader->maxBytes[$fileName][1]);
+            $this->assertSame($fileSize, $this->maxBytes[$fileName][1]);
 
             foreach (['targets', 'unclaimed'] as $role) {
                 $fileName = isset($version) ? "$version.$role.json" : "$role.json";
 
                 $this->assertInstanceOf(TargetsMetadata::class, $repository->getTargets($version, $role));
-                $this->assertSame(Repository::MAX_BYTES, $loader->maxBytes[$fileName][0]);
+                $this->assertSame(Repository::MAX_BYTES, $this->maxBytes[$fileName][0]);
 
                 $fileSize = filesize($baseDir . '/' . $fileName);
                 $this->assertInstanceOf(TargetsMetadata::class, $repository->getTargets($version, $role, $fileSize));
-                $this->assertSame($fileSize, $loader->maxBytes[$fileName][1]);
+                $this->assertSame($fileSize, $this->maxBytes[$fileName][1]);
             }
         }
     }
