92fe027de162f0eb71b6f223a1ee8a22988c02b0
Adam <adam@phenaproxima.net>
2023-03-24 14:55:46 -0400

    Update TUF spec to v1.0.29 (#325)

--- src/Key.php
+++ src/Key.php
@@ -38,7 +38,7 @@ final class Key
      *
      * @return static
      *
-     * @see https://theupdateframework.github.io/specification/v1.0.28#document-formats
+     * @see https://theupdateframework.github.io/specification/v1.0.29#document-formats
      */
     public static function createFromMetadata(array $keyInfo): self
     {
@@ -81,7 +81,7 @@ final class Key
      * @return string
      *     The key ID in hex format for the key metadata hashed using sha256.
      *
-     * @see https://theupdateframework.github.io/specification/v1.0.28#document-formats
+     * @see https://theupdateframework.github.io/specification/v1.0.29#document-formats
      *
      * @todo https://github.com/php-tuf/php-tuf/issues/56
      */
