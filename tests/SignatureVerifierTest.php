55326cfdf2410913859a5d20f63979976f922861
Phéna Proxima <adam@phenaproxima.net>
2023-03-27 15:00:30 -0400

    Adjust tests

--- tests/SignatureVerifierTest.php
+++ tests/SignatureVerifierTest.php
@@ -4,22 +4,21 @@ namespace Tuf\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
-use Tuf\KeyDB;
+use Tuf\Client\SignatureVerifier;
 use Tuf\Metadata\RootMetadata;
 use Tuf\Tests\TestHelpers\FixturesTrait;
 
 /**
- * @coversDefaultClass \Tuf\KeyDB
+ * @coversDefaultClass \Tuf\Client\SignatureVerifier
  */
-class KeyDBTest extends TestCase
+class SignatureVerifierTest extends TestCase
 {
     use FixturesTrait;
     use ProphecyTrait;
 
     /**
      * @covers ::createFromRootMetadata
-     *
-     * @return void
+     * @covers ::addKey
      */
     public function testCreateFromRootMetadata(): void
     {
@@ -31,12 +30,17 @@ class KeyDBTest extends TestCase
         $rootMetadata = RootMetadata::createFromJson(file_get_contents($rootJsonPath));
         self::assertInstanceOf(RootMetadata::class, $rootMetadata);
         $rootMetadata->trust();
-        $keyDb = KeyDB::createFromRootMetadata($rootMetadata);
-        self::assertInstanceOf(KeyDB::class, $keyDb);
+
+        $verifier = SignatureVerifier::createFromRootMetadata($rootMetadata);
+        $verifierKeys = new \ReflectionProperty($verifier, 'keys');
+        $verifierKeys->setAccessible(TRUE);
+        $verifierKeys = $verifierKeys->getValue($verifier);
+
         // Get the first key for comparison.
         $keys = $rootMetadata->getKeys();
         $key = reset($keys);
-        $retrievedKey = $keyDb->getKey($key->getComputedKeyId());
+        $this->assertArrayHasKey($key->getComputedKeyId(), $verifierKeys);
+        $retrievedKey = $verifierKeys[key($keys)];
         // Ensure the retrieved key is the same.
         self::assertSame($key->getPublic(), $retrievedKey->getPublic());
         self::assertSame($key->getType(), $retrievedKey->getType());
