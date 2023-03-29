b39fe99fe000f1fed7b000ac4bd9c41f3bb16370
Phéna Proxima <adam@phenaproxima.net>
2023-03-29 13:08:44 -0400

    some cleanliness

--- tests/Client/TestLoaderTrait.php
+++ tests/Client/TestLoaderTrait.php
@@ -6,6 +6,9 @@ use GuzzleHttp\Psr7\Utils;
 use Psr\Http\Message\StreamInterface;
 use Tuf\Exception\RepoFileNotFound;
 
+/**
+ * Defines a trait to load files from a simulated, in-memory server.
+ */
 trait TestLoaderTrait
 {
     /**
@@ -21,37 +24,36 @@ trait TestLoaderTrait
     protected array $fileContents = [];
 
     /**
-     * The $maxBytes argument passed to ::load() each time it was called.
+     * The $maxBytes arguments passed to ::load(), keyed by file name.
      *
-     * This is used by tests to confirm that the updater passes the expected
-     * file names and maximum download lengths.
+     * This is used to confirm that the updater passes the expected file names
+     * and maximum download lengths.
      *
-     * @var array[]
+     * @var int[][]
      */
     protected array $maxBytes = [];
 
     /**
-     * TestRepo constructor.
+     * Populates $this->fileContents with a fixture's server-side metadata.
      *
      * @param string $basePath
      *   The path of the fixture to read from.
      */
     public function populateFromFixture(string $basePath): void
     {
-        // Store all the repo files locally so they can be easily altered.
-        // @see self::setRepoFileNestedValue()
+        // Store the file contents in memory so they can be easily altered.
         $fixturesPath = "$basePath/server";
-        $repoFiles = glob("$fixturesPath/metadata/*.json");
+        $files = glob("$fixturesPath/metadata/*.json");
         $targetsPath = "$fixturesPath/targets";
         if (is_dir($targetsPath)) {
-            $repoFiles = array_merge($repoFiles, glob("$targetsPath/*"));
+            $files = array_merge($files, glob("$targetsPath/*"));
         }
-        foreach ($repoFiles as $repoFile) {
-            $baseName = basename($repoFile);
-            if (isset($this->fileContents[$baseName])) {
+        foreach ($files as $file) {
+            $baseName = basename($file);
+            if (array_key_exists($baseName, $this->fileContents)) {
                 throw new \UnexpectedValueException("For testing fixtures target files should not use metadata file names");
             }
-            $this->fileContents[$baseName] = file_get_contents($repoFile);
+            $this->fileContents[$baseName] = file_get_contents($file);
         }
     }
 
@@ -63,8 +65,6 @@ trait TestLoaderTrait
         $this->maxBytes[$locator][] = $maxBytes;
 
         if (array_key_exists($locator, $this->fileContents)) {
-            // Allow test code to directly set the returned stream so that they
-            // can be mocked.
             return Utils::streamFor($this->fileContents[$locator]);
         } else {
             throw new RepoFileNotFound("File $locator not found.");
