349cbdc35fc6551b76d27cf9d1c37d02c5f69ca0
Phéna Proxima <adam@phenaproxima.net>
2023-03-29 15:49:11 -0400

    Split testVerifiedDownload() into its own class

--- tests/Client/UpdaterTest.php
+++ tests/Client/UpdaterTest.php
@@ -2,14 +2,11 @@
 
 namespace Tuf\Tests\Client;
 
-use GuzzleHttp\Psr7\Utils;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Tuf\CanonicalJsonTrait;
 use Tuf\Client\Repository;
 use Tuf\Exception\DownloadSizeException;
 use Tuf\Exception\MetadataException;
 use Tuf\Exception\NotFoundException;
-use Tuf\Exception\Attack\InvalidHashException;
 use Tuf\Exception\Attack\SignatureThresholdException;
 use Tuf\Exception\RepoFileNotFound;
 use Tuf\Exception\TufException;
@@ -22,50 +19,7 @@ use Tuf\Tests\TestHelpers\UtilsTrait;
 abstract class UpdaterTest extends ClientTestBase
 {
     use CanonicalJsonTrait;
-    use ProphecyTrait;
     use UtilsTrait;
-
-    /**
-     * Tests that TUF will transparently verify downloaded target hashes.
-     *
-     * @covers ::download
-     */
-    public function testVerifiedDownload(): void
-    {
-        $this->loadClientAndServerFilesFromFixture('Simple');
-        $updater = $this->getUpdater();
-
-        $testFilePath = static::getFixturePath('Simple', 'server/targets/testtarget.txt', false);
-        $testFileContents = file_get_contents($testFilePath);
-        $this->assertSame($testFileContents, $updater->download('testtarget.txt')->getContents());
-
-        // If the file fetcher returns a file stream, the updater should NOT try
-        // to read the contents of the stream into memory.
-        $stream = $this->prophesize('\Psr\Http\Message\StreamInterface');
-        $stream->getMetadata('uri')->willReturn($testFilePath);
-        $stream->getContents()->shouldNotBeCalled();
-        $stream->rewind()->shouldNotBeCalled();
-        $stream->getSize()->willReturn(strlen($testFileContents));
-        $updater->download('testtarget.txt');
-
-        // If the target isn't known, we should get an exception.
-        try {
-            $updater->download('void.txt');
-            $this->fail('Expected a NotFoundException to be thrown, but it was not.');
-        } catch (NotFoundException $e) {
-            $this->assertSame('Target not found: void.txt', $e->getMessage());
-        }
-
-        $stream = Utils::streamFor('invalid data');
-        $this->serverFiles['testtarget.txt'] = $stream;
-        try {
-            $updater->download('testtarget.txt');
-            $this->fail('Expected InvalidHashException to be thrown, but it was not.');
-        } catch (InvalidHashException $e) {
-            $this->assertSame("Invalid sha256 hash for testtarget.txt", $e->getMessage());
-            $this->assertSame($stream, $e->getStream());
-        }
-    }
 
     /**
      * Tests that TUF transparently verifies targets signed by delegated roles.
