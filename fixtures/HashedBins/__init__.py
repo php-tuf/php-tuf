3545cb301b9340e1d4d860a9ac870f6aa361ecdc
Phéna Proxima <adam@phenaproxima.net>
2023-04-11 16:18:11 -0400

    Generate sketchy fixture

--- fixtures/HashedBins/__init__.py
+++ fixtures/HashedBins/__init__.py
@@ -6,5 +6,18 @@ def build():
     builder = FixtureBuilder('HashedBins')\
         .publish(with_client=True)
 
+    public_key = builder._keys['targets']['public'][0]
+    private_key = builder._keys['targets']['private'][0]
+
+    builder.repository.targets.delegate_hashed_bins([], [public_key])
+
     for c in list(string.ascii_lowercase):
-        builder.create_target(c + '.txt', signing_role=None)
+        name = c + '.txt'
+        builder.create_target(name, signing_role=None)
+        builder.repository.targets.add_target_to_bin(name)
+
+    for name in builder.repository.targets.get_delegated_rolenames():
+        builder.repository.targets(name).load_signing_key(private_key)
+        builder.repository.targets(name).add_verification_key(public_key)
+
+    builder.publish()
