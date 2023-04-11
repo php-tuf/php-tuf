50bc55630787fa35f18038b4a3c9713b87c35cc2
Adam <adam@phenaproxima.net>
2023-04-11 13:58:08 -0400

    Allow DelegatedRole to validate and carry path_hash_prefixes (#337)

--- src/DelegatedRole.php
+++ src/DelegatedRole.php
@@ -3,9 +3,13 @@
 
 namespace Tuf;
 
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Optional;
 use Symfony\Component\Validator\Constraints\Required;
 use Symfony\Component\Validator\Constraints\Type;
+use Tuf\Exception\MetadataException;
 
 /**
  * Class that represents a Delegated TUF role.
@@ -26,10 +30,11 @@ class DelegatedRole extends Role
      * @param string $name
      * @param int $threshold
      * @param array $keyIds
-     * @param array $paths
+     * @param array|null $paths
+     * @param array|null $pathHashPrefixes
      * @param bool $terminating
      */
-    private function __construct(string $name, int $threshold, array $keyIds, protected array $paths, protected bool $terminating)
+    private function __construct(string $name, int $threshold, array $keyIds, protected ?array $paths, protected ?array $pathHashPrefixes, protected bool $terminating)
     {
         parent::__construct($name, $threshold, $keyIds);
     }
@@ -45,16 +50,46 @@ class DelegatedRole extends Role
                 ]
             ),
             'terminating' => new Required(new Type('boolean')),
-            'paths' => new Required(new Type('array')),
+            // `paths` is mutually exclusive with `path_hash_prefixes`.
+            // @see ::validate()
+            'paths' => new Optional([
+                new Type('array'),
+                new All([
+                    new Type('string'),
+                    new NotBlank(),
+                ]),
+            ]),
+            'path_hash_prefixes' => new Optional([
+                new Type('array'),
+                new All([
+                    new Type('string'),
+                    new NotBlank(),
+                ]),
+            ]),
         ];
         static::validate($roleInfo, $roleConstraints);
         return new static(
             $roleInfo['name'],
             $roleInfo['threshold'],
             $roleInfo['keyids'],
-            $roleInfo['paths'],
+            $roleInfo['paths'] ?? null,
+            $roleInfo['path_hash_prefixes'] ?? null,
             $roleInfo['terminating']
         );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected static function validate(array $data, Collection $constraints): void
+    {
+        parent::validate($data, $constraints);
+
+        // Either `paths` or `path_hash_prefixes` MUST be specified, but not
+        // both.
+        if (!(array_key_exists('paths', $data) xor array_key_exists('path_hash_prefixes', $data))) {
+            throw new MetadataException('Either paths or path_hash_prefixes must be specified, but not both.');
+        }
     }
 
     /**
--- tests/Unit/DelegatedRoleTest.php
+++ tests/Unit/DelegatedRoleTest.php
@@ -3,6 +3,7 @@
 namespace Tuf\Tests\Unit;
 
 use Tuf\DelegatedRole;
+use Tuf\Exception\MetadataException;
 use Tuf\Role;
 
 /**
@@ -25,24 +26,6 @@ class DelegatedRoleTest extends RoleTest
     {
         parent::testCreateFromMetadata();
         self::assertFalse($this->role->isTerminating());
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function providerInvalidMetadata(): array
-    {
-        return [
-            'nothing' => [[]],
-            'no paths' => [
-                [
-                    'name' => 'a role',
-                    'threshold' => 1,
-                    'keyids' => ['good_key'],
-                    'terminating' => false,
-                ],
-            ],
-        ];
     }
 
     /**
@@ -115,5 +98,67 @@ class DelegatedRoleTest extends RoleTest
             ],
         ];
         return DelegatedRole::createFromMetadata($data);
+    }
+
+    public function testNoPathsOrPrefixes(): void
+    {
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessage('Either paths or path_hash_prefixes must be specified, but not both.');
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+        ]);
+    }
+
+    public function testPathsAndPrefixes(): void
+    {
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessage('Either paths or path_hash_prefixes must be specified, but not both.');
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+            'paths' => [],
+            'path_hash_prefixes' => [],
+        ]);
+    }
+
+    /**
+     * @testWith ["paths", "Not an array!", ["paths"], "This value should be of type array."]
+     *   ["paths", [""], ["paths", 0], "This value should not be blank."]
+     *   ["paths", [38], ["paths", 0], "This value should be of type string."]
+     *   ["path_hash_prefixes", "Not an array!", ["path_hash_prefixes"], "This value should be of type array."]
+     *   ["path_hash_prefixes", [""], ["path_hash_prefixes", 0], "This value should not be blank."]
+     *   ["path_hash_prefixes", [38], ["path_hash_prefixes", 0], "This value should be of type string."]
+     */
+    public function testPathsAndPrefixesMustBeArrays(string $key, mixed $value, array $propertyPath, string $expectedError): void
+    {
+        $propertyPath = preg_quote('[' . implode('][', $propertyPath) . ']');
+        $expectedError = preg_quote($expectedError);
+
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessageMatches("/Array$propertyPath:\s*$expectedError/");
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+            $key => $value,
+        ]);
     }
 }
