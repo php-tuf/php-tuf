b391ddf1c747c1111d2b09ef57babce1e29e6376
Phéna Proxima <adam@phenaproxima.net>
2023-04-11 11:51:19 -0400

    Add test coverage of validation in DelegatedRole

--- tests/Unit/DelegatedRoleTest.php
+++ tests/Unit/DelegatedRoleTest.php
@@ -3,6 +3,7 @@
 namespace Tuf\Tests\Unit;
 
 use Tuf\DelegatedRole;
+use Tuf\Exception\MetadataException;
 use Tuf\Role;
 
 /**
@@ -97,5 +98,62 @@ class DelegatedRoleTest extends RoleTest
             ],
         ];
         return DelegatedRole::createFromMetadata($data);
+    }
+
+    public function testNoPathsOrPrefixes(): void
+    {
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessage('Either paths or path_hash_prefixes must be specified, but not both.');
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+        ]);
+    }
+
+    public function testPathsAndPrefixes(): void
+    {
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessage('Either paths or path_hash_prefixes must be specified, but not both.');
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+            'paths' => [],
+            'path_hash_prefixes' => [],
+        ]);
+    }
+
+    /**
+     * @param string $key
+     *
+     * @testWith ["paths"]
+     *   ["path_hash_prefixes"]
+     */
+    public function testPathsAndPrefixesMustBeArrays(string $key): void
+    {
+        $this->expectException(MetadataException::class);
+        $this->expectExceptionMessageMatches("/Array\[$key\]:\s*This value should be of type array\./");
+
+        $this->createTestRole([
+            'name' => 'my_role',
+            'threshold' => 1000,
+            'keyids' => [
+                'good_key_1',
+                'good_key_2',
+            ],
+            'terminating' => false,
+            $key => 'Not an array!',
+        ]);
     }
 }
