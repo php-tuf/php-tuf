68c022de5197ebd70d5479775801db473cf34d06
Phéna Proxima <adam@phenaproxima.net>
2023-04-12 09:51:48 -0400

    Reduce the number of hash bins for sanity

--- fixtures/HashedBins/__init__.py
+++ fixtures/HashedBins/__init__.py
@@ -6,19 +6,14 @@ def build():
     builder = FixtureBuilder('HashedBins', { 'use_snapshot_length': True })\
         .publish(with_client=True)
 
-    public_key = builder._keys['targets']['public'][0]
-    private_key = builder._keys['targets']['private'][0]
+    public_key, private_key = builder._import_key()
 
-    builder.repository.targets.delegate_hashed_bins([], [public_key])
+    builder.repository.targets.delegate_hashed_bins([], [public_key], 8)
 
     for c in list(string.ascii_lowercase):
         name = c + '.txt'
         builder.create_target(name, signing_role=None)
-        builder.repository.targets.add_target_to_bin(name)
-
-    for name in builder.repository.targets.get_delegated_rolenames():
-        builder.repository.targets(name).load_signing_key(private_key)
-        builder.repository.targets(name).add_verification_key(public_key)
+        builder.repository.targets.add_target_to_bin(name, 8)
 
     builder.invalidate()
     builder.publish()
