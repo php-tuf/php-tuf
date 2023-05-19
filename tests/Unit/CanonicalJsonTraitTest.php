0732850f220bffadfd4aa8c5b29a22957c58a69f
Phéna Proxima <adam@phenaproxima.net>
2023-05-19 10:50:23 -0400

    Move coverage around

--- tests/Unit/CanonicalJsonTraitTest.php
+++ tests/Unit/CanonicalJsonTraitTest.php
@@ -22,28 +22,25 @@ class CanonicalJsonTraitTest extends TestCase
         $unsortedData = json_decode(file_get_contents("$fixturesDirectory/unsorted.json"), true, 512, JSON_THROW_ON_ERROR);
         static::sortKeys($unsortedData);
         $this->assertSame($unsortedData, $sortedData);
-    }
 
-    /**
-     * Indexed arrays with >= 10 items should not be changed.
-     */
-    public function testIndexedArraysAreNotChanged(): void
-    {
-        $original = [
-          'b' => array_fill(0, 20, 'Hello!'),
-          'a' => 'Canonically speaking, I go before b.',
+        // Indexed arrays should not be sorted in alphabetical order, at any
+        // level.
+        $data = [
+            // This must have at least 10 items, since that will be sorted
+            // alphabetically (which is what we're trying to avoid).
+            'b' => array_fill(0, 20, 'Hello!'),
+            'a' => 'Canonically speaking, I go before b.',
         ];
         // The associative keys should be in their original, non-canonical
         // order.
-        $this->assertSame(['b', 'a'], array_keys($original));
-        $this->assertTrue(array_is_list($original['b']));
-        $canonical = static::encodeJson($original);
+        $this->assertSame(['b', 'a'], array_keys($data));
+        $this->assertTrue(array_is_list($data['b']));
 
-        $canonical = static::decodeJson($canonical);
+        static::sortKeys($data);
         // The associative keys should be in canonical order now, and the
         // nested, indexed array should be unchanged.
-        $this->assertSame(['a', 'b'], array_keys($canonical));
-        $this->assertSame($original['b'], $canonical['b']);
+        $this->assertSame(['a', 'b'], array_keys($data));
+        $this->assertTrue(array_is_list($data['b']));
     }
 
     /**
