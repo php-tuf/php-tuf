d724d11e8d570f9696d53285a84e46c48b458184
Adam <adam@phenaproxima.net>
2024-03-13 10:49:57 -0400

    Remove DelegatedRole::isTerminating() in favor of read-only property (#377)

--- src/DelegatedRole.php
+++ src/DelegatedRole.php
@@ -17,14 +17,6 @@ use Tuf\Exception\MetadataException;
 class DelegatedRole extends Role
 {
     /**
-     * @return bool
-     */
-    public function isTerminating(): bool
-    {
-        return $this->terminating;
-    }
-
-    /**
      * DelegatedRole constructor.
      *
      * @param string $name
@@ -34,7 +26,7 @@ class DelegatedRole extends Role
      * @param array|null $pathHashPrefixes
      * @param bool $terminating
      */
-    private function __construct(string $name, int $threshold, array $keyIds, protected ?array $paths, protected ?array $pathHashPrefixes, protected bool $terminating)
+    private function __construct(string $name, int $threshold, array $keyIds, protected ?array $paths, protected ?array $pathHashPrefixes, public readonly bool $terminating)
     {
         parent::__construct($name, $threshold, $keyIds);
     }
