php-tuf/php-tuf

commit b77d2f0a8a7ca141919967fda3b388f372aaca23

Statically cache MetadataBase::createFromJson() (#390)

src/Metadata/MetadataBase.php
@@ -47,6 +47,10 @@ abstract class MetadataBase
 
     public readonly int $version;
 
+    /**
+     * @var array<string, static>
+     */
+    private static array $jsonCache = [];
 
     /**
      * MetadataBase constructor.
@@ -100,9 +104,13 @@ abstract class MetadataBase
      */
     public static function createFromJson(string $json): static
     {
+        $key = hash('xxh3', $json);
+        if (isset(static::$jsonCache[$key])) {
+            return static::$jsonCache[$key];
+        }
         $data = static::decodeJson($json);
         static::validate($data, new Collection(static::getConstraints()));
-        return new static($data, $json);
+        return static::$jsonCache[$key] = new static($data, $json);
     }
 
     /**

tests/Metadata/UntrustedExceptionTrait.php
@@ -8,12 +8,17 @@ trait UntrustedExceptionTrait
     /**
      * Tests that accessing method that with untrusted metadata throws an exception.
      *
+     * Because MetadataBase::createFromJson() has a static cache, this test
+     * must be run in a separate process so that there are no side-effects
+     * between test iterations.
+     *
      * @param string $method
      *   The method to call.
      * @param array $args
      *   The arguments for the method.
      *
      * @dataProvider providerUntrustedException
+     * @runInSeparateProcess
      */
     public function testUntrustedException(string $method, array $args = []): void
     {

commit 69aa338385f187cb0c1db9f39205643075371894

Fix regression caused by #390 (#397)

src/Metadata/MetadataBase.php
@@ -104,13 +104,17 @@ abstract class MetadataBase
      */
     public static function createFromJson(string $json): static
     {
+        // Maintain a static cache of instances created via the JSON storage.
+        // Because instances can be changed after being returned from this
+        // method, e.g. in TargetsMetadata::createFromJson(), ensure they are
+        // always cloned first.
         $key = hash('xxh3', $json);
-        if (isset(static::$jsonCache[$key])) {
-            return static::$jsonCache[$key];
+        if (!isset(static::$jsonCache[$key])) {
+            $data = static::decodeJson($json);
+            static::validate($data, new Collection(static::getConstraints()));
+            static::$jsonCache[$key] = new static($data, $json);
         }
-        $data = static::decodeJson($json);
-        static::validate($data, new Collection(static::getConstraints()));
-        return static::$jsonCache[$key] = new static($data, $json);
+        return clone static::$jsonCache[$key];
     }
 
     /**

tests/Metadata/UntrustedExceptionTrait.php
@@ -8,17 +8,12 @@ trait UntrustedExceptionTrait
     /**
      * Tests that accessing method that with untrusted metadata throws an exception.
      *
-     * Because MetadataBase::createFromJson() has a static cache, this test
-     * must be run in a separate process so that there are no side-effects
-     * between test iterations.
-     *
      * @param string $method
      *   The method to call.
      * @param array $args
      *   The arguments for the method.
      *
      * @dataProvider providerUntrustedException
-     * @runInSeparateProcess
      */
     public function testUntrustedException(string $method, array $args = []): void
     {